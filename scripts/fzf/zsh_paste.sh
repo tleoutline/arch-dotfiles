465a9c8e980f2a93be6672e1d5f1627a9b375643
tleoutline <tleoutline@gmail.com>
2024-02-14 07:43:56 +0700

    (script/fzf): Add fzf path picker widget for zsh

---
+++ scripts/fzf/zsh_paste.sh
@@ -0,0 +1,33 @@
+# fzf path pickers to insert to zsh
+# use in zsh with ctrl-f
+
+function _fzm_paste_command {
+    local directory="$1"
+    LBUFFER="${LBUFFER}$directory"
+    zle reset-prompt > /dev/null 2>&1
+}
+
+fzp() {
+  local result
+  local fzf_args=(
+    --height 40%
+    --reverse
+    --ansi \
+    --bind "ctrl-h:reload(fd -H --no-ignore)" \
+    --bind "ctrl-b:reload(fd -H -E '.git')" \
+    +m \
+    --header="ctrl-h:show hidden  ctrl-b: hide hidden" \
+    --tac
+    )
+    
+  
+  local lines=$(fd -Htd -E '.git' | fzf $fzf_args)
+  local result=$(tail -1 <<< "$lines")
+
+  if [[ -n "$result" ]]; then
+    eval -- "_fzm_paste_command \"\$result\""
+  fi
+}
+
+zle -N fzp
+bindkey  fzp
