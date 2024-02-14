dfc84565adecdc74f8ab68d838404ce063d1cc1b
tleoutline <tleoutline@gmail.com>
2024-02-14 07:42:35 +0700

    (script/fzf): Add control hint header

--- scripts/fzf/zoxide.sh
+++ scripts/fzf/zoxide.sh
@@ -1,14 +1,21 @@
 # fzf picker for zoxide under curent dir
-# ignore .git folder
+ 
 fz() {
   local result
-  fzf_args=(
+  local fzf_args=(
+    --height 40%
+    --reverse
+    --tac
+    --ansi \
     --bind "ctrl-h:reload(fd -Htd --no-ignore)" \
     --bind "ctrl-b:reload(fd -Htd -E '.git')" \
     +m \
     --preview "lsd --tree --depth=2 --color always --icon always --group-dirs=first -1 {}" \
+    --header="enter:jump  ctrl-h:show hidden  ctrl-b: hide hidden" \
     )
-  result=$(fd -Htd -E '.git' | fzf $fzf_args)
+    
+  local lines=$(fd -Htd -E '.git' | fzf $fzf_args)
+  local result=$(tail -1 <<< "$lines")
 
   if [[ -n "$result" ]]; then
     z "$result"
